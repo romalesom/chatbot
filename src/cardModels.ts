04578016bc2244b0e556fa16a229c3985733db21
Roman Sommer <rsommer@Admins-MacBook-Air-6.fritz.box>
2024-11-25 15:46:57 +0100

    Postman to Teams

--- src/cardModels.ts
+++ src/cardModels.ts
@@ -1,29 +1,35 @@
 /**
- * Adaptive Card data model for the default card.
+ * Adaptive Card data model for the card without responsible.
+ * responsible: the person who assigned you
  */
-export interface DefaultCardData {
+export interface Responsibilities {
   title: string;
   workItem: string;
   responsible: string;
+  comment?: string;
   url: string;
 }
 
 /**
- * Adaptive Card data model for the card without responsible.
+ * Adaptive Card data model for the default card.
+ * workItem: the activity you are required for
  */
-export interface NoResponsibleCardData {
+export interface Activities {
   title: string;
-  workItem?: string;
+  workItem: string;
   responsible?: string;
+  comment?: string;
   url: string;
 }
 
 /**
  * Adaptive Card data model for the card without work item.
+ * 
  */
-export interface NoWorkItemCardData {
+export interface Following {
   title: string;
-  responsible?: string;
-  workItem?: string;
+  responsible: string;
+  workItem: string;
+  comment: string;
   url: string;
 }
